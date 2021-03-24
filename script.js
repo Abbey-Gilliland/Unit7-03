443826b1ffb2f3b22cd35f512f964c1971e15ae4
Abbey Gilliland <abbey.gilliland@ocsbstudent.ca>
2021-03-24 13:51:24 +0000

    added comments.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+// This code sets the starting point of the variable for age.
 let age = 0
-
+// This code allows the button to activate the function "decide".
 document.getElementById('submit').addEventListener('click', decide)
-
+// This code takes the value of the text box, turns it into a number and assigns it to our variable.
 function decide () {
   age = document.getElementById('your-age').value
   age = parseInt(age)
+  // The code goes through if and else statements, so that if the variable of age is in a certain number range, the code will suggest the rating of movie that is best.
   if (age > 17) {
     document.getElementById('your-movie').innerHTML = 'You can watch a R rated movie alone.'
   } else if (age >= 13) {
